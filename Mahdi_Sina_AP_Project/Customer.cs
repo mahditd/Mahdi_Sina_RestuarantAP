c997a220f24257050ac9ce2419b9bf60efce3e7b
Sina Fallahi <sinafallahi005@gmail.com>
2024-06-26 13:42:11 +0330

    added Customer.cs by sina

---
+++ Mahdi_Sina_AP_Project/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahdi_Sina_AP_Project
+{
+    class Customer
+    {
+        public customer()
+        {
+            string userName;
+            string passWord;
+
+        }
+    }
+}
