dd799c5ce18d1409c03bdc688eaca8e84992e5fe
mahditd <149570042+mahditd@users.noreply.github.com>
2024-06-26 11:19:26 +0330

    Classes Created

---
+++ Mahdi_Sina_AP_Project/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sina_Mahdi_RestaurantAP
+{
+    class Admin
+    {
+        private string userName;
+        private string password;
+    }
+}
---
+++ Mahdi_Sina_AP_Project/Food And Order.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public enum PaymentMethod { Online, OnDelivery }
+
+
+namespace Sina_Mahdi_RestaurantAP
+{
+    class Food
+    {
+        private string name;
+
+        private double price;
+
+        private float rate;  // from 0 to 5
+
+        private string imagePath;
+
+        private Restaurant restaurant;
+
+        private string ingredients;
+
+
+        public Food(string name, double price, float rate, string imagePath, Restaurant restaurant, string ingredients)
+        {
+            this.name = name;
+            this.price = price;
+            this.rate = rate;
+            this.imagePath = imagePath;
+            this.restaurant = restaurant;
+            this.ingredients = ingredients;
+
+        }
+    }
+    class Order : Food
+    {
+        public DateTime orderDateTime = new DateTime();
+
+        PaymentMethod method;
+        public Order(string name, double price, float rate, string imagePath, Restaurant restaurant, string ingredients, PaymentMethod method)
+            : base(name, price, rate, imagePath, restaurant, ingredients)
+        {
+            this.orderDateTime = DateTime.Now;
+            this.method = method;
+        }
+
+    }
+}
