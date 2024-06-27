fa75a87e837bf39d8406bddbae6823c181e3173b
mahditd <149570042+mahditd@users.noreply.github.com>
2024-06-27 17:35:07 +0330

    merged into develop

--- Mahdi_Sina_AP_Project/obj/Debug/net8.0-windows/Mahdi_Sina_AP_Project.AssemblyInfo.cs
+++ Mahdi_Sina_AP_Project/obj/Debug/net8.0-windows/Mahdi_Sina_AP_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mahdi_Sina_AP_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64e1fde8ade4242970c7f9b9192713af5a1803ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cdc1a1ffb7bd8c836d813c1d1dcbd4155081d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mahdi_Sina_AP_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mahdi_Sina_AP_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
