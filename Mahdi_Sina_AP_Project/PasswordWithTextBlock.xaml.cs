b3db9ddbddaa18f38638514ca07f8910c00cd871
Sina Fallahi <sinafallahi005@gmail.com>
2024-06-28 13:54:17 +0330

    password box replaced to text box in getting password and confirming it

---
+++ Mahdi_Sina_AP_Project/PasswordWithTextBlock.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mahdi_Sina_AP_Project
+{
+    /// <summary>
+    /// Interaction logic for PasswordWithTextBlock.xaml
+    /// </summary>
+    public partial class PasswordWithTextBlock : UserControl
+    {
+        public PasswordWithTextBlock()
+        {
+            InitializeComponent();
+        }
+        private string placeHolder;
+
+        public string PlaceHolder
+        {
+            get { return placeHolder; }
+            set
+            {
+                placeHolder = value;
+                txtBlock.Text = placeHolder;
+            }
+        }
+
+        private void password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (password.Password != "")
+            {
+                txtBlock.Visibility = Visibility.Hidden;
+            }
+            else { txtBlock.Visibility = Visibility.Visible; }
+        }
+    }
+}
