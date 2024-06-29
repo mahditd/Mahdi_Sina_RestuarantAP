ec4f101cb0e57311b4b54f85c163d8c340402aba
Sina Fallahi <sinafallahi005@gmail.com>
2024-06-30 00:04:17 +0330

    commit before pulling

--- Mahdi_Sina_AP_Project/obj/Debug/net8.0-windows/Mahdi_Sina_AP_Project.AssemblyInfo.cs
+++ Mahdi_Sina_AP_Project/obj/Debug/net8.0-windows/Mahdi_Sina_AP_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mahdi_Sina_AP_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b294b79d71b56169b33bd770fb71babbe851e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23bcc56a69910ff164f616fcfc065136e5623f90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mahdi_Sina_AP_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mahdi_Sina_AP_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
