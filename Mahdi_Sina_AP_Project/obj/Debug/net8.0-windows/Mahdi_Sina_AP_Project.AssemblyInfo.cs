d0d8e97ef32889587688eff591bbd99535c7cb41
mahditd <149570042+mahditd@users.noreply.github.com>
2024-07-07 01:15:48 +0330

    all restaurants added

--- Mahdi_Sina_AP_Project/obj/Debug/net8.0-windows/Mahdi_Sina_AP_Project.AssemblyInfo.cs
+++ Mahdi_Sina_AP_Project/obj/Debug/net8.0-windows/Mahdi_Sina_AP_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mahdi_Sina_AP_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba8a48381a53e3ff79420fd9e270f6ded232dc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c47697cc9b304b85c871e6f2e2347c79792b85c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mahdi_Sina_AP_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mahdi_Sina_AP_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
